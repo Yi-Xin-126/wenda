d9c361227644153e202ba5f12216d240d174c762
Yi-Xin-126 <760645841@qq.com>
2017-02-08 15:18:23 +0800

    2.1

--- src/main/java/com/example/controller/IndexController.java
+++ src/main/java/com/example/controller/IndexController.java
@@ -1,16 +1,20 @@
 package com.example.controller;
 
+import com.example.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 @Controller
 public class IndexController {
-    @RequestMapping(path = {"/","/index"})
+
+    @RequestMapping(path = {"/","/index"}, method = {RequestMethod.GET})
     @ResponseBody
     public String index(){
 
@@ -21,10 +25,10 @@ public class IndexController {
     @ResponseBody
     public String profile(@PathVariable("userId") int userId,
                           @PathVariable("groupId") String groupId,
-                          @RequestParam(value = "type",defaultValue="1") int type,
-                          @RequestParam(value = "key",defaultValue="65") String key)
+                          @RequestParam(value = "type", defaultValue="1") int type,
+                          @RequestParam(value = "key", defaultValue="zz", required = false) String key)
     {
-        return String.format("Profile page of %s / %d,t: %d,k:%s",groupId,userId,type,key);
+        return String.format("Profile Page of %s / %d , t:%d , k:%s",groupId,userId,type,key);
     }
 
     @RequestMapping(path = {"/vm"},method = {RequestMethod.GET})
@@ -32,6 +36,13 @@ public class IndexController {
         model.addAttribute("value1","vvv1");
         List<String> colors = Arrays.asList(new String[]{"RED","GREEN","BLUE"});
         model.addAttribute("colors",colors);
+
+        Map<String,String> map = new HashMap<>();
+        for (int i= 0; i < 4; i++) {
+            map.put(String.valueOf(i),String.valueOf(i*i));
+        }
+        model.addAttribute("map",map);
+        model.addAttribute("user",new User("LEE"));
         return "home";
     }
 }
---
+++ src/main/java/com/example/model/User.java
@@ -0,0 +1,21 @@
+package com.example.model;
+
+/**
+ * Created by YiXin on 2017/2/8.
+ *
+ */
+public class User {
+    private String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
