167ce15bdfb8a1cedbdbafa50e5276dd664ae52c
YiXin <760645841@qq.com>
2017-02-14 16:44:39 +0800

    first commit

--- src/main/java/com/example/controller/HomeController.java
+++ src/main/java/com/example/controller/HomeController.java
@@ -9,6 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -23,6 +24,18 @@ import java.util.List;
 public class HomeController {
     private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 
+    private List<ViewObject> getQuestions(int userId, int offset, int limit) {
+        List<Question> questionList = questionService.getLatestQuestions(userId, offset, limit);
+        List<ViewObject> vos = new ArrayList<ViewObject>();
+        for (Question question : questionList) {
+            ViewObject vo = new ViewObject();
+            vo.set("question", question);
+            vo.set("user", userService.getUser(question.getUserId()));
+            vos.add(vo);
+        }
+        return vos;
+    }
+
     @Autowired
     UserService userService;
     @Autowired
@@ -31,15 +44,15 @@ public class HomeController {
     @RequestMapping(path = {"/","/index"}, method = {RequestMethod.GET})
     public String index(Model model){
 
-        List<Question> questionList = questionService.getLatestQuestions(0, 0, 10);
-        List<ViewObject> vos = new ArrayList<ViewObject>();
-        for (Question question : questionList) {
-            ViewObject vo = new ViewObject();
-            vo.set("question", question);
-            vo.set("user", userService.getUser(question.getUserId()));
-            vos.add(vo);
-        }
-        model.addAttribute("vos", vos);
+
+        model.addAttribute("vos", getQuestions(0, 0, 10));
+        return "index";
+    }
+
+    @RequestMapping(path = {"/user/{userId}"}, method = {RequestMethod.GET})
+    public String userIndex(Model model,
+                            @PathVariable("userId") int userId){
+        model.addAttribute("vos", getQuestions(userId, 0 ,10));
         return "index";
     }
 }
